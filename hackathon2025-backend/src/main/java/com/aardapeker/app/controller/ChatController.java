4da4993ff43cc2f96200306e88f13ec75b3ff2ef
aardapeker <a.ardapeker@gmail.com>
2025-09-10 14:22:16 +0300

    delete the redundant imports

--- hackathon2025-backend/src/main/java/com/aardapeker/app/controller/ChatController.java
+++ hackathon2025-backend/src/main/java/com/aardapeker/app/controller/ChatController.java
@@ -10,8 +10,6 @@ import java.util.List;
 import com.google.cloud.texttospeech.v1.ListVoicesResponse;
 import com.google.cloud.texttospeech.v1.TextToSpeechClient;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
